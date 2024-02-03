46713de6fad15830a63c97fe1a54eec1c55d3de1
Aswin246 <mashwinarjun@gmail.com>
2024-02-03 12:03:29 +0530

    Debounce customHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,34 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-function useTodos(n) {
-  const [todo, setTodo] = useState([]);
-  const [loading, setLoading] = useState(true);
+const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
   useEffect(() => {
-    const value = setInterval(() => {
-      const fetchData = async () => {
-        const response = await axios.get(
-          "https://sum-server.100xdevs.com/todos"
-        );
-        setTodo(response.data.todos);
-        setLoading(false);
-      };
-      fetchData();
-    }, n * 1000);
-    const fetchData = async () => {
-      const response = await axios.get("https://sum-server.100xdevs.com/todos");
-      setTodo(response.data.todos);
-      setLoading(false);
-    };
-    fetchData();
+    const timerId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
 
-    return () => {
-      clearInterval(value);
-    };
-  }, [n]);
-  return {
-    todo: todo,
-    loading: loading,
-  };
-}
+    return () => clearTimeout(timerId);
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+
 function App() {
-  // const [render, setRender] = useState(true);
-  const { todo, loading } = useTodos(5);
-
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     setRender((r) => !r);
-  //   }, 5000);
-  // }, []);
+  const [inputValue, setInputValue] = useState("");
+  const debouncedValue = useDebounce(inputValue, 500);
   return (
     <>
-      {/* {render ? <Component /> : <div>"Hello World"</div>} */}
-      {loading ? (
-        <div>Loading</div>
-      ) : (
-        <div>
-          {todo.map((todo) => (
-            <Todo key={todo.id} todo={todo} />
-          ))}
-        </div>
-      )}
+      <div>{debouncedValue}</div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Search..."
+      />
     </>
   );
 }
 
-function Todo({ todo }) {
-  return (
-    <>
-      <div>{todo.title}</div>
-      <div>{todo.description}</div>
-    </>
-  );
-}
-
-// function Component() {
-//   useEffect(() => {
-//     console.log("Mounting");
-
-//     return () => {
-//       console.log("Un Mounting");
-//     };
-//   }, []);
-//   return (
-//     <>
-//       <div>From the component</div>
-//     </>
-//   );
-// }
 export default App;
